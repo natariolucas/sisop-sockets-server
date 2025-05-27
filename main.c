a70eabbff5c610889d4b94f484dcc5939850c865
Lucas Natario <lucasenatario@gmail.com>
2025-05-27 00:10:38 -0300

    Resolve basics operations

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include <pthread.h>
 #include "semaphores.h"
 #include "server_socket.h"
+#include "regex.h"
 
 #define LISTEN_BACKLOG 128
 #define MAX_CONNECTIONS 5
@@ -16,6 +17,8 @@
 #define IP ""
 
 void* processSocketRequests(int acceptedSocketFD, int connectionSemaphore);
+char* getStringFromMatchIndex(regmatch_t* matches, int index, char* source);
+double* processOperationFromString(const char* operand1, char operator, const char* operand2);
 
 int main() {
     printf("[+] starting with IP: '%s' and PORT: %d\n", IP, PORT);
@@ -64,10 +67,25 @@ int main() {
 }
 
 void* processSocketRequests(int acceptedSocketFD, int connectionSemaphore) {
-    char buffer[1024];
+    char request[1024];
+    char response[1024];
+
+    const char* regexRule = "^([+-]?[0-9]+)[ ]*([+*/-])[ ]*([+-]?[0-9]+)[\r\n]*$";
+    regex_t regex;
+
+    int regexCompilationResult = regcomp(&regex, regexRule, REG_EXTENDED);
+    if (regexCompilationResult != 0 ) {
+        perror("[!] error while compiling regex\n");
+
+        close(acceptedSocketFD);
+        VSemaphore(connectionSemaphore);
+        regfree(&regex);
+
+        return NULL;
+    }
 
     while (true) {
-        ssize_t amountReceived = recv(acceptedSocketFD, buffer, sizeof(buffer), 0);
+        ssize_t amountReceived = recv(acceptedSocketFD, request, sizeof(request), 0);
         if (amountReceived < 0) {
             char errorMessage[1024];
 
@@ -82,15 +100,88 @@ void* processSocketRequests(int acceptedSocketFD, int connectionSemaphore) {
             break;
         }
 
-        buffer[amountReceived] = '\0';
-        printf("[+] fd: %d - received message: %s\n", acceptedSocketFD, buffer);
+        request[amountReceived] = '\0';
+        printf("Input a regex: %s", request);
+        regmatch_t matches[4];
 
-        char genericResponse[] = "ok";
-        send(acceptedSocketFD, genericResponse, sizeof (genericResponse), 0);
+        int regexResult = regexec(&regex, request, 4, matches, 0);
+        switch (regexResult) {
+            case 0: {
+                char *operand1 = getStringFromMatchIndex(matches, 1, request);
+                char *operator = getStringFromMatchIndex(matches, 2, request);
+                char *operand2 = getStringFromMatchIndex(matches, 3, request);
+
+                double *result = processOperationFromString(operand1, *operator, operand2);
+                if (result == NULL) {
+                    strcpy(response, "error al procesar la operación, verifique su expresión");
+
+                    free(operand1);
+                    free(operator);
+                    free(operand2);
+                    free(result);
+                    break;
+                }
+
+                sprintf(response, "RESULT: %.2f", *result);
+
+                free(operand1);
+                free(operator);
+                free(operand2);
+                free(result);
+                break;
+            }
+            case REG_NOMATCH:
+                strcpy(response, "expresión inválida, verifique la sintaxis de su expresión");
+                break;
+            default:
+                strcpy(response, "error al validar su expresion, intente nuevamente");
+        }
+
+        send(acceptedSocketFD, response, sizeof (response), 0);
     }
 
     close(acceptedSocketFD);
     VSemaphore(connectionSemaphore);
+    regfree(&regex);
 
     return NULL;
+}
+
+char* getStringFromMatchIndex(regmatch_t* matches, int index, char* source) {
+    long long startOffset = matches[index].rm_so;
+    int charCount = (int)(matches[index].rm_eo - startOffset);
+    char* result = malloc(sizeof(char) * (charCount + 1)); // +1 for end of string
+    strncpy(result, source + startOffset, charCount);
+    result[charCount] = '\0';
+
+    return result;
+}
+
+double* processOperationFromString(const char* operand1, const char operator, const char* operand2) {
+    char* endPointerOfParseOperand1;
+    const double op1 = strtod(operand1, &endPointerOfParseOperand1);
+
+    char* endPointerOfParseOperand2;
+    const double op2 = strtod(operand2, &endPointerOfParseOperand2);
+
+    double* result = malloc(sizeof(double));
+
+    switch (operator) {
+        case '+':
+            *result = op1 + op2;
+            break;
+        case '-':
+            *result = op1 - op2;
+            break;
+        case '*':
+            *result = op1 * op2;
+            break;
+        case '/':
+            *result = op1 / op2;
+            break;
+        default:
+            return NULL;
+    }
+
+    return result;
 }
